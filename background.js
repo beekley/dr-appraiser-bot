43ced98e8669544b7ab42113591310df5c698126
Brett Beekley <brettbeekley@gmail.com>
2017-06-29 10:00:17 -0700

    Get avg, low and high values

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.commands.onCommand.addListener(function(command) {
     console.log(selection[0]);
     
     // Create URL from selection
-    const url = 'http://www.ebay.com/sch/i.html?_nkw=' + encodeURIComponent(selection[0]) + '&LH_Sold=1&_ipg=50&LH_Complete=1'
+    const url = 'https://www.ebay.com/sch/i.html?_nkw=' + encodeURIComponent(selection[0]) + '&LH_Sold=1&_ipg=20&LH_Complete=1'
     
     console.log(url);
     
@@ -22,19 +22,61 @@ chrome.commands.onCommand.addListener(function(command) {
         const prices = [];
 
         // Add each price to the array
+        // Using jquery here for some reason tho
         $('.bidsold', this.responseText).each(function(i, price) {
           prices.push($(this).text().trim());
         })
 
-        console.log(prices);
+        filteredPrices = filterOutliers(extractPrice(prices));
+        console.log(filteredPrices);
+        console.log('Average:', sum(filteredPrices)/filteredPrices.length);
+        console.log('Low:', min(filteredPrices));
+        console.log('High:', max(filteredPrices));
       }
     }
 
     xhr.send();
     
-    
-    
   });
   
-  
 });
+
+// Returns median of a set of numbers
+// Sorts the array in-place as well 
+function median(array) {
+  return array.sort()[Math.floor(array.length / 2)]
+}
+
+// Maps an array of price strings to an array of floats
+function extractPrice(array) {
+  const exp = /[0-9]{1,3}(?:,?[0-9]{3})*\.[0-9]{2}$/
+  
+  return array.map((price) => {
+    return price.match(exp) ? Number(price.match(exp)[0]) : null;
+  });
+}
+
+// Removes data that is +/- n% of the median
+function filterOutliers(array) {
+  const n = 0.5;
+  const med = median(array);
+  
+  return array.filter((price) => {
+    return price && (price < (med * 1/n) && price > (med * n));
+  })
+}
+
+function sum(array) {
+  return array.reduce((acc, el) => {
+    return acc + el;
+  })
+}
+
+function max(array) {
+  return Math.max.apply(Math, array);
+}
+
+function min(array) {
+  return Math.min.apply(Math, array);
+}
+
